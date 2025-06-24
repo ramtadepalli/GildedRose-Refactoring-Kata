6a7c893cbcfdc900474730aacad2247d59582ecd
ramtadepalli <trk571@gmail.com>
2025-06-24 19:18:38 +0530

    Test Class File Execution

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
+
+import sys
+import os
 import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
+        print("This Is Sample Unit Test Method")
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
-
+        self.assertEqual("foo", items[0].name)
         
 if __name__ == '__main__':
     unittest.main()
